31a98fc7074c24e9663ae5f0e6f8324ced71f1a7
LilysPictures <lili-g-semenova@yandex.ru>
2021-07-13 19:06:27 +0300

    теперь точно удалила

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,9 @@
 <template>
 
 <div id="app">
-  <!-- <AppLayout>
-  <router-link to="/"> HOME </router-link>
-  <router-link :to="{name: 'About'}"> About </router-link> 
-  <router-link :to="{name: 'Header'}"> Header </router-link> -->
+  <!-- <router-link :to="{name: 'About'}"> About </router-link>  -->
+ 
   <app-header/>
-
-  <!-- <v-btn
-      @click="clickBtn"
-     
-  /> -->
-  <!-- <img src='./assets/BG_MM_InversGraff.jpg' class="BG" v-if='showImage' /> -->
-<!-- <button @click="clickBtn"></button> -->
-
-
-
-<!-- <div class="ghost">	
-  <img src="./assets/ghost.png" alt="ghost" style="width: 300px; height: 300px; bottom: 0; position: absolute; z-index: 12000" id="idGhost">	
- </div> -->
-
-
-<!-- 
-<img @click="show = !show" src="./assets/left_web.png" style="width: 200px; height: 180px">
-
-<transition
-   name="custom-classes-transition"
-    enter-active-class="animated tada"
-    leave-active-class="animated bounceOutRight"
-  >
-  <img src="./assets/spider.png" style="width: 200px; height: 180px" v-if="show">     
-  </transition>  -->
-
   <spider/>
   <router-link :to="{name: 'Osya'}"> <img src='./assets/Osya.png' style="width: 200px; height: 180px; margin-left: 10vw; margin-top: 20vh"/> </router-link> 
   <router-view/>
